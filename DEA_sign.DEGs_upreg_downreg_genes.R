a07853276efba9b903c01249f10bd0a126009198
Prashansha Goel <prashanshagoeljoy@gmail.com>
2025-11-27 13:27:50 +0530

    Refactor DEA analysis script for clarity and structure

--- DEA_sign.DEGs_upreg_downreg_genes.R
+++ DEA_sign.DEGs_upreg_downreg_genes.R
@@ -2,6 +2,7 @@ setwd("E:/Bioteck_Project/combined result/GSE86374/cel.files")
 getwd()
 
 # Differential Expression Analysis
+
 library(limma)
 
 norm_data_1 <- read.csv("normalized_data_1.csv", row.names = 1)
@@ -15,10 +16,12 @@ top_genes <- topTable(fit, coef = 2, number = Inf)
 write.csv(top_genes, file = "DEA_result.csv")
 
 # To get significicant differentially expresed genes
+
 significant_genes <- top_genes[top_genes$adj.P.Val<0.05,]
 write.csv(significant_genes, file = "sign_DEG.csv")
 
 #To get upregulated and downregulated genes
+
 library(dplyr)
 
 input_file <- "sign_DEG.csv"
@@ -37,5 +40,6 @@ downregulated_genes <- sign_genes%>%
 
 write.csv(upregulated_genes, file = output_1, row.names = FALSE, quote = FALSE)
 write.csv(downregulated_genes, file = output_2, row.names = FALSE, quote = FALSE)
+
 
 
